f1d612123426faeb8eb645734abdfee54c8df0d5
Mike Jongen <mikejongen@hotmail.com>
2020-12-19 16:54:02 +0100

    pt 1 + 2

--- src/day-10-adapter-array.cr
+++ src/day-10-adapter-array.cr
@@ -23,5 +23,41 @@ OptionParser.parse do |parser|
 end
 
 unless file_name.empty?
+  data_str = File.read_lines(file_name)
+  data = [] of Int64
+  data_str.each do |value|
+    data << value.to_i64
+  end
+
+  data << 0
+  data = data.sort
+  data << data[-1] + 3
+
+
+  difference = [0, 0, 0, 0]
+  (1...data.size).each do |index|
+    diff = data[index] - data[index-1]
+    difference[diff] += 1
+  end
+
+  puts difference[1] * difference[3]
+
+  previous = Array(Int64|Nil).new(data.size, nil)
+  puts arrangements(data, 0, previous)
+
+end
+
+def arrangements(data : Array(Int64), index : Int64, previous : Array(Int64|Nil))
+  return previous[index] if previous[index]
+  total = 0_i64
+  index_next = index + 1
+  until (index_next >= data.size) || (data[index_next] > (data[index] + 3))
+    total += 1 if (index_next == (data.size - 1))
+    total += arrangements(data, index_next, previous).as(Int64)
+    index_next += 1
+  end
+  previous[index] = total
+  return total if total
+  return 0
 end
 
